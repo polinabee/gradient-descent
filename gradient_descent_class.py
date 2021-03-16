0d08b8297be54d0ed30215347b25231c4576cb9c
Polina Berenstein <polinaberen@gmail.com>
2021-03-16 10:45:34 +0100

    syntax updates

--- gradient_descent_class.py
+++ gradient_descent_class.py
@@ -2,17 +2,18 @@ import numpy as np
 
 
 class GradientDescent:
-    def __init__(self, data_params=(4, 0.4, 1000)):
-        self.d = data_params[0]  # dimensions
-        self.mu = data_params[1]
-        self.n = data_params[2]
+    def __init__(self,
+                 data_params=(4, 0.4, 1000),
+                 step=0.1,
+                 loss='hinge'):
+        self.mu, self.d, self.n = data_params
         self.data = self.generate_data()
         self.x_train, self.x_test, self.y_train, self.y_test = self.test_train_split()
+        self.theta_init = np.random.uniform(0.0, 1.0, size=self.d)
+        self.tuning_param = self.set_loss_function(loss)
 
     def generate_data(self):
-        mu = self.mu
-        d = self.d
-        n = self.n
+        mu, d, n = self.mu, self.d, self.n
         mean_a = [mu] + [0] * (d - 1)  # mean vector
         mean_b = [-mu] + [0] * (d - 1)
         cov = np.identity(d)  # identity covariance
@@ -22,18 +23,24 @@ class GradientDescent:
         x_vals = np.concatenate((x_vals_a, x_vals_b), axis=0)
         return np.concatenate((x_vals, y_vals), axis=1)
 
-    def test_train_split(self):
+    def test_train_split(self, split=0.7):
         data = self.data
+        n = self.n
         x = np.asarray([a[:-1] for a in data])
-        x_train, x_test = x[:int(self.n * 0.7)], x[int(self.n * 0.3):]
+        x_train, x_test = x[:int(n * split)], x[int(n * int(1 - split)):]
 
         y = np.asarray([a[-1] for a in data])
-        y_train, y_test = y[:int(self.n * 0.7)], y[int(self.n * 0.3):]
+        y_train, y_test = y[:int(n * split)], y[int(n * int(1 - split)):]
         return x_train, x_test, y_train, y_test
 
-
+    def set_loss_function(self, loss_name):
+        loss_functions = {'hinge': lambda x: max(0, 1 + x),
+                          'exp': lambda x: np.exp(x),
+                          'logistic': lambda x: np.log2(1 + np.exp(x))}
+        return loss_functions[loss_name]
 
 
 if __name__ == '__main__':
-    data_dims = (5, 3, 200)  # dimensions of the data distrubution
-    gd = GradientDescent(data_dims)
+    data_dims = (5, 3, 200)  # dimensions of the data distribution
+    step_size = 0.1
+    gd = GradientDescent(data_dims, step_size, 'hinge')
